0f55aa52902ba4bd553fb1d08884a1db73dce7dd
Artur <helter@wp.pl>
2022-04-19 17:26:05 +0200

    dispatch add activity

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
+import {toDoActions} from './store/todoSlice'
 
 function App() {
+  const [activity, setActivity] = useState('');
+  const dispatch = useDispatch();
+
+  const inputHandler= (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setActivity(e.target.value)
+  }
+
+  const addActivity = () => {
+    dispatch(toDoActions.add(activity))
+  }
+
   return (
     <div className="App">
       <h1>Your list of activities:</h1>
-      <input placeholder='Enter your activity'></input>
-      <button>Add</button>
+      <input value={activity} onChange={inputHandler} placeholder='Enter your activity'></input>
+      <button onClick={addActivity}>Add</button>
     </div>
   );
 }
